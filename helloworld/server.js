69ae39f6ed82932f201c319c050827dac32acd05
rockiearun <rockiearun@gmail.com>
2014-10-12 13:42:01 +0000

    Hello world example

---
+++ helloworld/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var server = http.createServer();
+server.on('request', function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World\n');
+})
+server.listen(process.env.PORT, process.env.IP);;
+console.log('Server started');
+/*
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(process.env.PORT, process.env.IP);
+*/
